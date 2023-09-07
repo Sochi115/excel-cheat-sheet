8bd9300dbc3cb290c9078357f55cdd798c86f713
Chiv Sochivoath <chiv.sochivoath@gmail.com>
2023-09-07 21:41:28 +0700

    Created simple HTMX GET

--- main.go
+++ main.go
@@ -15,6 +15,12 @@ type Film struct {
 func mainPage(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("templates/index.html"))
 
+	tmpl.Execute(w, nil)
+}
+
+func getFilms(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/db_items.html"))
+
 	films := map[string][]Film{
 		"Films": {
 			{Title: "The Godfather", Director: "Francis Ford Coppola"},
@@ -28,6 +34,7 @@ func mainPage(w http.ResponseWriter, r *http.Request) {
 func main() {
 	port := ":3333"
 	http.HandleFunc("/", mainPage)
+	http.HandleFunc("/get-movies", getFilms)
 	fmt.Println("Server Listening on port ", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
